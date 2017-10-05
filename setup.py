3a13ce5575d91dbf5066ab2898cbc016a023c295
Sebastian Bassi <sbassi@genesdigitales.com>
2017-10-04 18:08:02 -0700

    testing

--- setup.py
+++ setup.py
@@ -58,9 +58,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    ##entry_points={
-    ##    'console_scripts': [
-    ##        'sample=sample:main',
-    ##    ],
-    ##},
+    entry_points={
+        'console_scripts': [
+            'allofplos=allofplos.plos_corpus:__main__',
+        ],
+    },
 )
