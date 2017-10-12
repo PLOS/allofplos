f6c497a8ac21bc09ab2a3e9bed8c9c2d8c5e2144
Elizabeth Seiver <elizabeth.seiver@gmail.com>
2017-10-12 15:33:36 -0700

    fix download_updated_xml

--- allofplos/plos_corpus.py
+++ allofplos/plos_corpus.py
@@ -593,10 +593,11 @@ def download_updated_xml(article_file,
     url = URL_TMP.format(doi)
     articletree_remote = et.parse(url)
     articleXML_remote = et.tostring(articletree_remote, method='xml', encoding='unicode')
+    print(article_file)
     try:
-        articletree_local = et.parse(article_file)
+        articletree_local = et.parse(doi_to_path(article_file))
     except OSError:
-        article_file_alt = os.path.join(tempdir, os.path.basename(article_file))
+        article_file_alt = os.path.join(tempdir, os.path.basename(doi_to_path(article_file)))
         articletree_local = et.parse(article_file_alt)
     articleXML_local = et.tostring(articletree_local, method='xml', encoding='unicode')
 
