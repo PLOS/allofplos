59015fc45eb432309c8cf00ca63019e420de11f7
Elizabeth Seiver <eseiver@plos.org>
2017-12-13 15:09:38 -0800

    sample -> starter

--- allofplos/__init__.py
+++ allofplos/__init__.py
@@ -7,7 +7,7 @@ ALLOFPLOS_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 corpusdir = os.path.join(ALLOFPLOS_DIR_PATH, 'allofplos_xml')
 
 # Starter pack of PLOS articles
-sampledir = os.path.join(ALLOFPLOS_DIR_PATH, 'sample_corpus')
+starterdir = os.path.join(ALLOFPLOS_DIR_PATH, 'starter_corpus')
 
 # Temporary folder for downloading and processing new articles
 newarticledir = os.path.join(ALLOFPLOS_DIR_PATH, 'new_plos_articles')
