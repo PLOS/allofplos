8b9f802e925890e5bca8aab6cb3a7d7470fc27f6
Elizabeth Seiver <eseiver@plos.org>
2017-12-13 15:08:36 -0800

    + starterdir info in __init__

--- allofplos/__init__.py
+++ allofplos/__init__.py
@@ -6,13 +6,16 @@ ALLOFPLOS_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 # Main directory for the corpus of article XML files
 corpusdir = os.path.join(ALLOFPLOS_DIR_PATH, 'allofplos_xml')
 
+# Starter pack of PLOS articles
+sampledir = os.path.join(ALLOFPLOS_DIR_PATH, 'sample_corpus')
+
 # Temporary folder for downloading and processing new articles
 newarticledir = os.path.join(ALLOFPLOS_DIR_PATH, 'new_plos_articles')
 
 del os
 
-# NB: any packages that you want to expose at the top level, you will need to 
-# import after creating global variables that they may rely upon 
+# NB: any packages that you want to expose at the top level, you will need to
+# import after creating global variables that they may rely upon
 # (e.g., corpusdir)
 
 from .article_class import Article
