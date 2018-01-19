67970485f3cbe183f34bdd7fb181f73e0e0f824c
Elizabeth Seiver <elizabeth.seiver@gmail.com>
2018-01-18 19:07:34 -0800

    fix corpus.filepaths list method

--- allofplos/corpus_class.py
+++ allofplos/corpus_class.py
@@ -77,7 +77,7 @@ class Corpus():
     @property
     def filepaths(self):
         """List of article XML files in corpus directory, including the full path."""
-        return [os.path.join(self.directory, fname) for fname in self.iter_files]
+        return list(self.iter_filepaths)
 
     def random_dois(self, count):
         """
