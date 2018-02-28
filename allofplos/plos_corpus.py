a0502c957243c69dbf8178a6f32d27dc669e9f4f
Elizabeth Seiver <elizabeth.seiver@gmail.com>
2018-02-27 17:21:42 -0800

    change update function to allofplos.update
    deprecates allofplos.plos_corpus, updates README with new command

--- allofplos/plos_corpus.py
+++ allofplos/plos_corpus.py
@@ -1,4 +1,10 @@
+import warnings
+
 from .corpus.plos_corpus import main
 
 if __name__ == "__main__":
+    warnings.simplefilter('always', DeprecationWarning)
+    warnings.warn("This update method is deprecated. use 'python -m allofplos.update'",
+                  DeprecationWarning,
+                  stacklevel=2)
     main()
---
+++ allofplos/update.py
@@ -0,0 +1,4 @@
+from .corpus.plos_corpus import main
+
+if __name__ == "__main__":
+    main()
