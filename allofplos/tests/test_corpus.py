22653c72cf1c30316e17701a26e650f8735f75a2
M Pacer <mpacer@berkeley.edu>
2018-03-03 18:13:58 -0800

    use pytest fixture to define corpus

--- allofplos/tests/test_corpus.py
+++ allofplos/tests/test_corpus.py
@@ -2,17 +2,19 @@ from . import TESTDATADIR
 from .. import Corpus
 
 import random
+import pytest
 
-def test_corpus_instantiate():
-    corpus = Corpus(TESTDATADIR)
+@pytest.fixture
+def corpus():
+    return Corpus(TESTDATADIR, seed=1000)
+
+def test_corpus_instantiate(corpus):
     assert isinstance(corpus, Corpus)
 
-def test_corpus_len():
-    corpus = Corpus(TESTDATADIR)
+def test_corpus_len(corpus):
     assert len(corpus) == 5
 
-def test_corpus_iterator():
-    corpus = Corpus(TESTDATADIR)
+def test_corpus_iterator(corpus):
     article_dois = {article.doi for article in corpus}
     assert article_dois == {
         '10.1371/annotation/3155a3e9-5fbe-435c-a07a-e9a4846ec0b6',
@@ -22,7 +24,7 @@ def test_corpus_iterator():
         '10.1371/journal.pbio.2001413',
     }
 
-def test_corpus_random_article():
+def test_corpus_random_article(corpus):
     corpus = Corpus(TESTDATADIR, seed=1000)
     article = corpus.random_article
     assert article.doi == "10.1371/journal.pone.0185809"
